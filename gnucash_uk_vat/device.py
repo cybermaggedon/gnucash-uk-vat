12d7066dfd48da50baa26c91fb2e217b6d2edfd8
Cyber MacGeddon <cybermaggedon@gmail.com>
2022-06-27 14:12:29 +0100

    Added MacOS device ID

--- gnucash_uk_vat/device.py
+++ gnucash_uk_vat/device.py
@@ -1,5 +1,20 @@
 
+import platform
+
 def get_device():
+
+    p = platform.system()
+
+    if p == 'Linux':
+        return get_linux_device()
+    elif p == 'Darwin':
+        return get_darwin_device()
+    elif p == 'Windows':
+        return get_windows_device()
+    else:
+        raise RuntimeError("Can't do get_device on platform " + p)
+
+def get_linux_device():
     try:
         import dmidecode
         d = dmidecode.DMIDecode(command=["sudo",  "dmidecode"])
@@ -14,4 +29,23 @@ def get_device():
     except Exception as e:
         print(e)
         return None
+
+def get_windows_device():
+    raise RuntimeError("Windows not implemented")
+
+def get_darwin_device():
+    import json
+    import subprocess
+    system_profile_data = subprocess.Popen(
+        ['system_profiler', '-json', 'SPHardwareDataType'],
+        stdout=subprocess.PIPE
+    )
+    data = json.loads(system_profile_data.stdout.read())
+    sp = data.get('SPHardwareDataType', {})[0]
+
+    return {
+        "manufacturer": "Apple",
+        "model": sp.get("machine_model"),
+        "serial": sp.get("serial_number"),
+    }
 
