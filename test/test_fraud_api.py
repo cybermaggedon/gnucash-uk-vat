0433159ad177d703cb0913da172ec53c0720b07d
Sam Bull <git@sambull.org>
2025-08-06 01:31:22 +0100

    Update test_fraud_api.py

--- test/test_fraud_api.py
+++ test/test_fraud_api.py
@@ -11,21 +11,14 @@ import gnucash_uk_vat.hmrc as hmrc
 from gnucash_uk_vat.config import Config
 from gnucash_uk_vat.auth import Auth
 
-# Command-line argument parser
 parser = argparse.ArgumentParser(description="Gnucash to HMRC VAT API")
 parser.add_argument('--config', '-c',
-            default=Path('config.json'), type=Path,
+                    default=Path('config.json'), type=Path,
                     help='Configuration file (default: ./config.json)')
-
-# Parse arguments
 args = parser.parse_args()
 
-# Loads default config, user and auth
-config = Config(args.config)
-a = Auth()
-
 # Use the sandbox service
-svc = hmrc.VatTest(config, a, None)
+svc = hmrc.VatTest(Config(args.config), Auth(), None)
 
 # Call fraud API and dump out results.
 # This is expected to fail on lack of gov-client-multi-factor headers.
